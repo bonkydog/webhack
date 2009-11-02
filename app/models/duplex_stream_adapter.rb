d36dab9ef4244791d8560977e27cc5e371d3d525
bonkydog <bonkydog@bonkydog.com>
2009-11-02 14:52:34 -0800

    switched to singleton attribute for marking streams ready.

--- app/models/duplex_stream_adapter.rb
+++ app/models/duplex_stream_adapter.rb
@@ -14,36 +14,53 @@ class DuplexStreamAdapter
   self.logger.level = $DEBUG ? Logger::DEBUG : Logger::INFO
    
   def initialize(coming_down, coming_up, going_down, going_up)
+    
     @coming_down = coming_down
     @coming_up = coming_up
     @going_down = going_down
     @going_up = going_up
+
+    [@coming_down, @coming_up, @going_down, @going_up].each {|s| add_ready_attribute_to(s)}
+
+    @incoming_streams = [@coming_up, @coming_down]
+    @outgoing_streams = [@going_up, @going_down]
   end
 
-  def read_if_ready(readable_streams, source, buffer)
-    if readable_streams.include?(source)
-      coming_up_character = source.sysread(1)[0]
-      logger.debug "coming_up_character=#{coming_up_character}"
-      buffer.push( coming_up_character)
+  def add_ready_attribute_to(stream)
+    stream.instance_eval  do
+      @ready = false
+      class << self
+        attr_accessor :ready
+        alias :ready? :ready 
+      end
     end
   end
 
-  def write_if_ready(writable_streams, sink, buffer)
+  def read_if_ready(source, buffer)
+    return unless source.ready?
+    coming_up_character = source.sysread(1)[0]
+    logger.debug "coming_up_character=#{coming_up_character}"
+    buffer.push( coming_up_character)
+  end
+
+  def write_if_ready(sink, buffer)
     return if buffer.empty? 
-    return unless writable_streams.include?(sink)
+    return unless sink.ready?
     going_up_character = buffer.shift
     logger.debug "going_up_character=#{going_up_character}"
     sink.syswrite( going_up_character.chr)
   end
 
   def select_readable
-    select_result = IO.select([@coming_up, @coming_down], nil, nil, SELECT_READABLE_TIMEOUT_SECONDS)
-    select_result ? select_result.flatten : []
+    select_result = IO.select(@incoming_streams, nil, nil, SELECT_READABLE_TIMEOUT_SECONDS)
+    readable_streams = select_result ? select_result[0] : []
+    @incoming_streams.each {|s| s.ready = readable_streams.include?(s)}
   end
 
   def select_writable
-    select_result = IO.select(nil, [@going_up, @going_down], nil, SELECT_WRITABLE_TIMEOUT_SECONDS)
-    select_result ? select_result.flatten : []
+    select_result = IO.select(nil, @outgoing_streams, nil, SELECT_WRITABLE_TIMEOUT_SECONDS)
+    writable_streams = select_result ? select_result[1] : []
+    @outgoing_streams.each {|s| s.ready = writable_streams.include?(s)}
   end
 
   def adapt
@@ -59,13 +76,13 @@ class DuplexStreamAdapter
 
         readable_streams = select_readable
 
-        read_if_ready(readable_streams, @coming_up, upward_buffer)
-        read_if_ready(readable_streams, @coming_down, downward_buffer)
+        read_if_ready(@coming_up, upward_buffer)
+        read_if_ready(@coming_down, downward_buffer)
 
         writable_streams = select_writable
 
-        write_if_ready(writable_streams, @going_up, upward_buffer)
-        write_if_ready(writable_streams, @going_down, downward_buffer)
+        write_if_ready(@going_up, upward_buffer)
+        write_if_ready(@going_down, downward_buffer)
 
       rescue EOFError => e
         logger.debug "Ignoring EOF: #{e.inspect}"
--- spec/models/duplex_stream_adapter_spec.rb
+++ spec/models/duplex_stream_adapter_spec.rb
@@ -3,148 +3,194 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe DuplexStreamAdapter do
 
   before do
-    @adapter = DuplexStreamAdapter.new(:coming_down, :coming_up, :going_down, :going_up)
+    @coming_down = Object.new
+    @coming_up = Object.new
+    @going_down = Object.new
+    @going_up = Object.new
+    @adapter = DuplexStreamAdapter.new(@coming_down, @coming_up, @going_down, @going_up)
   end
 
-  describe "#read_if_ready" do
+  describe "#add_ready_attribute" do
     before do
-      stub(@stream = Object.new).sysread(1) {[?X.to_i]}.subject
+      @stream = Object.new
+      @adapter.add_ready_attribute_to @stream
+    end
+
+    it "should add a ready attribute to an object" do
+      @stream.ready = true
+      @stream.should be_ready
+    end
+
+    it "should not add a ready attribute to the object's class" do
+      Object.new.should_not respond_to(:ready)
+    end
+  end
+
+  describe "io" do
+    before do
+      @stream = Object.new
+      @adapter.add_ready_attribute_to(@stream)
+      stub(@stream).sysread(1) {[?X.to_i]}.subject
+      stub(@stream).syswrite
+
       @buffer = []
     end
 
-    context "when stream is ready to be read" do
 
-      before do
-        @readable_streams = [@stream]
-        @adapter.read_if_ready(@readable_streams, @stream, @buffer)
+    describe "#read_if_ready" do
+
+      context "when stream is ready to be read" do
+        before do
+          @stream.ready = true
+          @adapter.read_if_ready(@stream, @buffer)
+        end
+
+        it "should read a character from the stream" do
+          @stream.should have_received.sysread(1)
+        end
+
+        it "should write the character read from the stream to the end of the buffer" do
+          @buffer.first.should == ?X
+        end
       end
 
-      it "should read a character from the stream" do
-        @stream.should have_received.sysread(1)
+      context "when stream is not ready to be read" do
+        before do
+          @adapter.read_if_ready(@stream, @buffer)
+        end
+
+        it "should not change the buffer" do
+          @buffer == []
+        end
+
+        it "should not attempt to read from the stream" do
+          @stream.should_not have_received.sysread(anything)
+        end
+      end
+    end
+
+    describe "#write_if_ready" do
+
+      context "when  the buffer has characters" do
+        before do
+          @buffer = [?x, ?y, ?z, ?z, ?y]
+        end
+
+        context "when stream is ready to be written" do
+          before do
+            @stream.ready = true
+            @adapter.write_if_ready(@stream, @buffer)
+          end
+
+
+          it "should remove the character from the front of the buffer" do
+            @buffer.should == [?y, ?z, ?z, ?y]
+          end
+
+          it "should write the character at the front of the buffer to the stream" do
+            @stream.should have_received.syswrite('x')
+          end
+        end
+
+        context "when stream is not ready to write" do
+          before do
+            @buffer = [?x, ?y, ?z, ?z, ?y]
+            @adapter.write_if_ready(@stream, @buffer)
+          end
+
+          it "should not change the buffer" do
+            @buffer == []
+          end
+
+          it "should not attempt to write to the stream" do
+            @stream.should_not have_received.syswrite(anything)
+          end
+        end
       end
 
-      it "should write the character read from the stream to the end of the buffer" do
-        @buffer.first.should == ?X
+
+      context "when the buffer is empty" do
+        before do
+          @stream.ready = true
+          @adapter.write_if_ready(@stream, @buffer)
+        end
+
+        it "should not change the buffer" do
+          @buffer == []
+        end
+
+        it "should not attempt to write to the stream" do
+          @stream.should_not have_received.syswrite(anything)
+        end
       end
 
     end
 
-    context "when stream is not ready to be read" do
-      before do
-        @readable_streams = []
-        @adapter.read_if_ready(@readable_streams, @stream, @buffer)
+    describe "#select_readable" do
+
+      context "when a stream is ready" do
+        before do
+          stub(IO).select([@coming_up, @coming_down], nil, nil, 10) {[[@coming_up], [], []]} # timeout should be 10sec
+          @adapter.select_readable
+        end
+
+        it "should return mark the readable streams ready" do
+          @coming_up.should be_ready
+        end
+
+        it "should return not mark the unreadable streams ready" do
+          @coming_down.should_not be_ready
+        end
       end
 
-      it "should not change the buffer" do
-        @buffer == []
-      end
+      context "when no stream is ready" do
+        before do
+          stub(IO).select([@coming_up, @coming_down], nil, nil, 10) {nil} # timeout should be 10 seconds
+          @adapter.select_readable
+        end
 
-      it "should not attempt to read from the stream" do
-        @stream.should_not have_received.sysread(anything)
-      end
-
-    end
-
-  end
-
-  describe "#write_if_ready" do
-    before do
-      stub(@stream = Object.new).syswrite
-    end
-
-    context "when stream is ready to be written to and the buffer has characters" do
-      before do
-        @buffer = [?x, ?y, ?z, ?z, ?y]
-        @writable_streams = [@stream]
-        @adapter.write_if_ready(@writable_streams, @stream, @buffer)
-      end
-
-      it "should remove the character from the front of the buffer" do
-        @buffer.should == [?y, ?z, ?z, ?y]
-      end
-
-      it "should write the character at the front of the buffer to the stream" do
-        @stream.should have_received.syswrite('x')
-      end
-
-
-    end
-
-    context "when stream is not ready to be read" do
-      before do
-        @buffer = [?x, ?y, ?z, ?z, ?y]
-        @writable_streams = []
-        @adapter.write_if_ready(@writable_streams, @stream, @buffer)
-      end
-
-
-      it "should not change the buffer" do
-        @buffer == []
-      end
-
-      it "should not attempt to write to the stream" do
-        @stream.should_not have_received.syswrite(anything)
-      end
-    end
-
-    context "when the buffer is empty" do
-      before do
-        @buffer = []
-        @writable_streams = [@stream]
-        @adapter.write_if_ready(@writable_streams, @stream, @buffer)
-      end
-
-
-      it "should not change the buffer" do
-        @buffer == []
-      end
-
-      it "should not attempt to write to the stream" do
-        @stream.should_not have_received.syswrite(anything)
-      end
-    end
-
-  end
-
-  describe "#select_readable" do
-
-    context "when a stream is ready" do
-      it "should return a arry of readable streams" do
-        stub(IO).select([:coming_up, :coming_down], nil, nil, 10) {[[:coming_up], [], []]}
-        @adapter.select_readable.should == [:coming_up]
-      end
-    end
-
-    context "when no stream is ready" do
-      it "should return an empty array" do
-        stub(IO).select([:coming_up, :coming_down], nil, nil, 10) {nil}
-        @adapter.select_readable.should == []
+        it "should return not mark the unreadable streams ready" do
+          @coming_up.should_not be_ready
+          @coming_down.should_not be_ready
+        end
       end
 
     end
 
-  end
+    describe "#select_writable" do
 
-  describe "#select_writable" do
+      context "when a stream is ready" do
+        before do
+          stub(IO).select(nil, [@going_up, @going_down], nil, 0) {[[], [@going_up], []]} # timeout should be 0 seconds
+          @adapter.select_writable
+        end
 
-    context "when a stream is ready" do
-      it "should return an array of writable streams" do
-        stub(IO).select(nil, [:going_up, :going_down], nil, 0) {[[], [:going_down], []]}
-        @adapter.select_writable.should == [:going_down]
+        it "should return mark the writable streams ready" do
+          @going_up.should be_ready
+        end
+
+        it "should return not mark the unwritable streams ready" do
+          @going_down.should_not be_ready
+        end
       end
-    end
 
-    context "when no stream is ready" do
-      it "should return an empty array" do
-        stub(IO).select(nil, [:going_up, :going_down], nil, 0) {nil}
-        @adapter.select_writable.should == []
+      context "when no stream is ready" do
+        before do
+          stub(IO).select(nil, [@going_up, @going_down], nil, 0) {nil} # timeout should be 0 seconds
+          @adapter.select_writable
+        end
+
+        it "should return not mark the unwritable streams ready" do
+          @going_up.should_not be_ready
+          @going_down.should_not be_ready
+        end
       end
-    end
 
+    end
   end
 
   describe "#adapt" do
-
+    it "should read from the incoming streams and write to the outgoing streams"
   end
 
 end
