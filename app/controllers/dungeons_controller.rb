3f7644e3535b4073bd6a55e939c789400fd4817a
bonkydog <bonkydog@bonkydog.com>
2009-11-03 12:38:56 -0800

    did dungeon_controller for real.

--- app/controllers/dungeons_controller.rb
+++ app/controllers/dungeons_controller.rb
@@ -1,23 +1,17 @@
 class DungeonsController < ActionController::Base
 
-#SPIKE  
-  
-#  def show
-#    @game = Game.find(params[:game_id])
-#  end
+  def edit
+    @game = Game.find(params[:game_id])
+    @game.transcript = "" if @game.transcript.blank?
+    @game.transcript += @game.look
+    @game.save!
+  end
 
-#  def edit
-#    @game = Game.find(params[:game_id])
-#    @game.transcript ||= ""
-#    @game.transcript += @game.look
-#    @game.save!
-#  end
-
-#  def update
-#    @game = Game.find(params[:game_id])
-#    move = params[:move]
-#    @game.move(move + "\n")
-#    redirect_to :action => "edit", :game_id => @game.id
-#  end
+  def update
+    @game = Game.find(params[:game_id])
+    move = params[:move]
+    @game.move(move + "\n")
+    redirect_to :action => "edit", :game_id => @game.id
+  end
 
 end
--- spec/controllers/dungeons_controller_spec.rb
+++ spec/controllers/dungeons_controller_spec.rb
@@ -4,10 +4,6 @@ describe DungeonsController do
 
   describe "subresource routes" do
 
-    it "should route to restful show" do
-      should route(:get, "/games/1/dungeon").to(:controller => :dungeons, :action => :show, :game_id => 1)
-    end
-
     it "should route to restful edit" do
       should route(:get, "/games/1/dungeon/edit").to(:controller => :dungeons, :action => :edit, :game_id => 1)
     end
@@ -17,5 +13,47 @@ describe DungeonsController do
     end
 
   end
+
+  describe "actions" do
+    before do
+      @game = Factory(:game)
+    end
+
+    describe "#edit" do
+      before do
+        stub.proxy(Game).find do |game|
+          stub(game).look {"fake output"}
+        end
+        get :edit, :game_id => @game.id
+      end
+
+      it "should fetch and assign the game for display" do
+        assigns(:game).should == @game
+      end
+
+      it "should inspect and record game state" do
+        @game.reload.transcript.should == "fake output"
+      end
+    end
+
+    describe "#update" do
+      before do
+        stub.proxy(Game).find do |game|
+          stub(@found_game = game).move(anything)
+        end
+        put :update, :game_id => @game.id, :move => "go east"
+      end
+
+      it "should relay the move to the game" do
+        @found_game.should have_received.move("go east\n")
+      end
+
+      it "should redirect to edit" do
+        response.should redirect_to(edit_game_dungeon_path(@game.id))
+      end
+
+    end
+
+  end 
 
 end
