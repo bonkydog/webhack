3a7fd8eb7a0ade322c77b55f77f5f25fb5cd05c5
bonkydog <bonkydog@bonkydog.com>
2009-11-03 23:12:51 -0800

    set up jasmine

---
+++ public/javascripts/spec/screen_spec.js
@@ -0,0 +1,12 @@
+describe('Screen', function () {
+
+  it('should have a passing test', function() {
+      expect(true).toEqual(true);
+  });
+
+  describe('Nested Describe', function () {
+     it('should also have a passing test', function () {
+        expect(true).toEqual(true);
+     });
+  });
+});
