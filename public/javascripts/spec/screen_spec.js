4edadb09e24e0c8a526b788764fcbc8eaa555121
bonkydog <bonkydog@bonkydog.com>
2009-11-05 00:29:12 -0800

    basic screen functionality complete.

--- public/javascripts/spec/screen_spec.js
+++ public/javascripts/spec/screen_spec.js
@@ -44,42 +44,103 @@ describe('screen', function () {
 
   describe("putCharacter", function () {
     it("should should put a character into the cell at the requested coordinates", function() {
-      screen.putCharacter("@", 12, 16); // ansi is one-based.
-      var cell = $("table.screen tr:eq(11) td:eq(15)"); // jquery selectors are zero-based.
+      screen.putCharacter("@", 12, 16); // ANSI escape is one-based.
+      var cell = $("table.screen tr:eq(11) td:eq(15)"); // jQuery selectors are zero-based.
       expect(cell.html()).toEqual("@");
     });
+
+    it("should not move the cursor", function() {
+      var cursor_before = screen.getCursor();
+      screen.putCharacter("^", 12, 16);
+      expect(screen.getCursor().row).toEqual(cursor_before.row);
+      expect(screen.getCursor().column).toEqual(cursor_before.column);
+    });
+
+    it("should escape demons", function() {
+      var cursor_before = screen.getCursor();
+      screen.putCharacter("&", 12, 16);
+      expect(screen.getCharacter(12,16)).toEqual("&amp;")
+    });
+
+    it("should escape up-stairs", function() {
+      var cursor_before = screen.getCursor();
+      screen.putCharacter("<", 12, 16);
+      expect(screen.getCharacter(12,16)).toEqual("&lt;")
+    });
+
+    it("should escape down-stairs", function() {
+      var cursor_before = screen.getCursor();
+      screen.putCharacter(">", 12, 16);
+      expect(screen.getCharacter(12,16)).toEqual("&gt;")
+    });
+
   });
 
   describe("getCharacter", function () {
     it("should get the character from the cell at the requested coordinates", function() {
-      screen.putCharacter("@", 5, 19); // ansi is one-based.
-      expect(screen.getCharacter(5, 19)).toEqual("@");
+      screen.putCharacter("*", 5, 19);
+      expect(screen.getCharacter(5, 19)).toEqual("*");
     });
+
+    it("should not move the cursor", function() {
+      var cursor_before = screen.getCursor();
+      screen.getCharacter(12, 16);
+      expect(screen.getCursor().row).toEqual(cursor_before.row);
+      expect(screen.getCursor().column).toEqual(cursor_before.column);
+    });
+
+
   });
 
   describe("writeChraracter", function () {
     describe("when the argument is an ordinary character", function () {
       it("should put the character in the current cell (pointed to by the cursor)", function() {
-
+        screen.setCursor(8,7);
+        screen.writeCharacter("d");
+        expect(screen.getCharacter(8, 7)).toEqual("d");
       });
 
       describe("when the cursor is not on the right or bottom edge", function () {
         it("should move the cursor one column to the right", function() {
-
+          screen.setCursor(9,6);
+          screen.writeCharacter("$");
+          expect(screen.getCursor().row).toEqual(9);
+          expect(screen.getCursor().column).toEqual(7);
         });
       });
       describe("when the cursor is on the right edge of the screen", function () {
         it("should move the cursor to the beginning of the next line", function() {
-
+          screen.setCursor(9,6);
+          screen.writeCharacter("L");
+          expect(screen.getCursor().row).toEqual(9);
+          expect(screen.getCursor().column).toEqual(7);
         });
       });
+
       describe("when the cursor is on the bottom edge of the screen", function () {
         it("should move the cursor one column to the right", function() {
-
+          screen.setCursor(9,80);
+          screen.writeCharacter("c");
+          expect(screen.getCursor().row).toEqual(10);
+          expect(screen.getCursor().column).toEqual(1);
         });
       });
+
+
       describe("when the cursor is in the bottom right corner of the screen", function () {
         it("should scroll everything up a row and move the cursor to the beginning of the new bottom row", function() {
+          screen.putCharacter("1", 1, 1);
+          screen.putCharacter("2", 2, 1);
+          screen.putCharacter("X", 25, 1);
+          screen.setCursor(25,80);
+          screen.writeCharacter("%");
+
+          expect(screen.getCursor().row).toEqual(25);
+          expect(screen.getCursor().column).toEqual(1);
+          expect(screen.getCharacter(1,1)).toEqual("2");
+          expect(screen.getCharacter(2,1)).toEqual("");
+          expect(screen.getCharacter(24,1)).toEqual("X");
+          expect(screen.getCharacter(24,80)).toEqual("%");
 
         });
       });
--- public/javascripts/src/screen.js
+++ public/javascripts/src/screen.js
@@ -6,17 +6,24 @@ WEBHACK.screen = function (container_selector, my){
   var cursor = {row: 1, column: 1};
   var container = $(container_selector).slice(0,1);
   var table;
+  var tbody;
+
+  var buildRow = function(tr){
+    for (row = 0; row < 80; ++row) {
+      tr.append($("<td>"));
+    }
+  };
 
   var build = function() {
+    tbody = $("<tbody>");
     table = $("<table>").addClass("screen");
+    table.append(tbody);
     container.append(table);
     var column, row;
     for (column = 0; column < 25; ++column) {
       var tr = $("<tr>");
-      table.append(tr);
-      for (row = 0; row < 80; ++row) {
-        tr.append($("<td>"));
-      }
+      tbody.append(tr);
+      buildRow(tr);
     }
   };
 
@@ -32,8 +39,7 @@ WEBHACK.screen = function (container_selector, my){
   };
 
   var findCell = function(row, column){
-    var selector = "tr:eq(" + (row - 1) + ") td:eq(" + (column - 1) + ")";
-    return table.contents(selector).slice(0,1);
+    return tbody.children().slice(row - 1, row).children().slice(column - 1, column);
   };
 
   var putCharacter = function(character, row, column){
@@ -42,6 +48,24 @@ WEBHACK.screen = function (container_selector, my){
 
   var getCharacter = function(row, column){
     return findCell(row, column).html();
+  };
+
+  var writeCharacter = function(character){
+    putCharacter(character, cursor.row, cursor.column);
+    cursor.column++;
+    if (cursor.column > 80) {
+      cursor.column = 1;
+      cursor.row++;
+
+      if (cursor.row > 25) {
+        cursor.row = 25;
+        var top_tr = tbody.children().slice(0,1);
+        top_tr.remove();
+        var tr = $("<tr>")
+        buildRow(tr);
+        tbody.append(tr);
+      }
+    }
   };
 
 
@@ -59,7 +83,8 @@ WEBHACK.screen = function (container_selector, my){
   self.setCursor = setCursor;
   self.putCharacter = putCharacter;
   self.getCharacter = getCharacter;
-
+  self.writeCharacter = writeCharacter;
+  
   return self;
 };
 
